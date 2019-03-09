f3173d1baeaad785c4de2521946b13ebc4944923
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-03-09 20:30:42 +0500

    Замена ссылки постела в alt на его название

--- src/render-card.js
+++ src/render-card.js
@@ -10,7 +10,7 @@ const renderCard = (cardData) => {
     <span class="film-card__duration">${cardData.duration}</span>
     <span class="film-card__genre">${cardData.genre}</span>
   </p>
-  <img src="${cardData.poster}" alt="${cardData.poster}" class="film-card__poster">
+  <img src="${cardData.poster}" alt="${cardData.filmTitle.release}" class="film-card__poster">
   <p class="film-card__description">${cardData.description}</p>
   <button class="film-card__comments">${cardData.comments} comments</button>
   ${(!cardData.hasControls) ? `` : `<form class="film-card__controls"><button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">${cardData.watchlist ? `WL` : `Add to watchlist`}</button><button class="film-card__controls-item button film-card__controls-item--mark-as-watched">${cardData.watched ? `WTCHD` : `Mark as watched`}</button><button class="film-card__controls-item button film-card__controls-item--favorite">${cardData.favorite ? `FAV` : `Mark as favorite`}</button></form>`}
