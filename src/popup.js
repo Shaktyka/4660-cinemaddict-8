274905c10a0e16e6f31c5ad22eb3aca31ccea957
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-03-21 23:02:55 +0500

    Черновая реализация рендеринга юзер-рейтинга

--- src/popup.js
+++ src/popup.js
@@ -9,6 +9,18 @@ const getCalendarDate = (date) => {
   return `${gotDate.getDate()} ${months[gotDate.getMonth() - 1]} ${gotDate.getFullYear()}`;
 };
 
+// Рендеринг списка элементов для рейтинга
+const renderRatingScale = (userScore, scoreBlock) => {
+  console.log(userScore, scoreBlock);
+  const fragment = document.createDocumentFragment();
+  for (let i = 1; i <= 9; i++) {
+    const scaleString = `<input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="${i}" id="rating-${i}" ${userScore === i ? `checked` : ``}><label class="film-details__user-rating-label" for="rating-${i}">${i}</label>`;
+    const scaleFragment = renderElement(scaleString);
+    fragment.appendChild(scaleFragment);
+  }
+  scoreBlock.appendChild(fragment);
+};
+
 class Popup {
   constructor(data) {
     console.log(data);
@@ -179,33 +191,10 @@ class Popup {
               <p class="film-details__user-rating-feelings">How you feel it?</p>
 
               <div class="film-details__user-rating-score">
-                <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="1" id="rating-1">
-                <label class="film-details__user-rating-label" for="rating-1">1</label>
-
-                <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="2" id="rating-2">
-                <label class="film-details__user-rating-label" for="rating-2">2</label>
-
-                <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="3" id="rating-3">
-                <label class="film-details__user-rating-label" for="rating-3">3</label>
-
-                <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="4" id="rating-4">
-                <label class="film-details__user-rating-label" for="rating-4">4</label>
-
-                <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="5" id="rating-5" checked>
-                <label class="film-details__user-rating-label" for="rating-5">5</label>
-
-                <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="6" id="rating-6">
-                <label class="film-details__user-rating-label" for="rating-6">6</label>
-
-                <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="7" id="rating-7">
-                <label class="film-details__user-rating-label" for="rating-7">7</label>
-
-                <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="8" id="rating-8">
-                <label class="film-details__user-rating-label" for="rating-8">8</label>
-
-                <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="9" id="rating-9">
-                <label class="film-details__user-rating-label" for="rating-9">9</label>
-
+                ${[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (`
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="${item}" id="rating-${item}" ${(item === this._userRating) ? `checked` : ``}>
+                  <label class="film-details__user-rating-label" for="rating-${item}">${item}</label>`.trim()
+                )).join('')}
               </div>
             </section>
           </div>
