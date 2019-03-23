f424a7abef367ced01db3d051678f93382799940
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-03-23 21:16:47 +0500

    Подготовлен Component

--- src/component.js
+++ src/component.js
@@ -1,18 +1,35 @@
 class Component {
-   constructor() {
-       if (new.target === Component) {
-           throw new Error(`Can't instantiate Component, only concrete one.`);
-       }
-   }
+  constructor() {
+    if (new.target === Component) {
+      throw new Error(`Can't instantiate Component, only concrete one.`);
+    }
 
-   get template() {
+    this._element = null;
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  get template() {
     throw new Error(`You have to define template.`);
   }
 
   render() {
-    this.unrender();
-    this._element = renderElement(this.template);
-    this.bind();
-    return this._element;
+    if (this._element) {
+      this._element = renderElement(this.template);
+      this.bind();
+      return this._element;
+    }
   }
+
+  bind() {};
+
+  unbind() {};
+
+  unrender() {
+    this.unbind();
+    this._element.remove();
+    this._element = null;
+  };
 }
